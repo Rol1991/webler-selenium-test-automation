baa34981d83b4f32698a32f3e7b0d189d9db7519
Szőke Roland <szokeroland0@gmail.com>
2024-04-13 10:18:12 +0200

    Initial commit

---
+++ src/main/java/huwebler/weblerseleniumtestautomation/WeblerSeleniumTestAutomationApplication.java
@@ -0,0 +1,13 @@
+package huwebler.weblerseleniumtestautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblerSeleniumTestAutomationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeblerSeleniumTestAutomationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/huwebler/weblerseleniumtestautomation/WeblerSeleniumTestAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package huwebler.weblerseleniumtestautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblerSeleniumTestAutomationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
